b061cee31c97d98376dc5a4d9bbb47c9fc7b7c3c
Matt Mendonca <matt.mendonca1@gmail.com>
2014-10-01 22:20:20 -0400

    tweaked config files

--- config.rb
+++ config.rb
@@ -1,8 +1,6 @@
-# Change this to :production when ready to deploy the CSS to the live server.
 #environment = :development
 environment = :production
 
-# Location of the theme's resources.
 css_dir         = "public/css"
 sass_dir        = "public/sass"
 images_dir      = "public/images"
@@ -10,6 +8,4 @@ images_dir      = "public/images"
 add_import_path "bower_components/foundation/scss"
 add_import_path "bower_components/foundation-icon-fonts"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
 output_style = (environment == :development) ? :expanded : :compressed
--- gulpfile.js
+++ gulpfile.js
@@ -12,48 +12,47 @@ var gulp = require('gulp'),
   });
 
 // Compass / SASS
-gulp.task('compass', function() {
-  gulp.src('public/sass/*.sass')
-    .pipe(compass({
-      config_file: 'config.rb',
-      css: 'public/css',
-      sass: 'public/sass'
-    }));
-});
+  gulp.task('compass', function() {
+    gulp.src('public/sass/*.sass')
+      .pipe(compass({
+        config_file: 'config.rb',
+        css: 'public/css',
+        sass: 'public/sass'
+      }));
+  });
 
-// Lint Task
-gulp.task('lint', function() {
-  return gulp.src('public/js/podium/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
+// Lint
+  gulp.task('lint', function() {
+    return gulp.src('public/js/podium/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+  });
 
-// Concatenate & Minify JS
 /*
-gulp.task('scripts', function() {
-  return gulp.src('public/js/podium/*.js')
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('public/js'))
-    .pipe(rename('all.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/js'));
-});
+// Concatenate & Minify JS
+  gulp.task('scripts', function() {
+    return gulp.src('public/js/podium/*.js')
+      .pipe(concat('all.js'))
+      .pipe(gulp.dest('public/js'))
+      .pipe(rename('all.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('public/js'));
+  });
 */
 
 // Minify JS
-gulp.task('scripts', function() {
-  return gulp.src('public/js/podium/*.js')
-    .pipe(uglify())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest('public/js'));
-});
+  gulp.task('scripts', function() {
+    return gulp.src('public/js/podium/*.js')
+      .pipe(uglify())
+      .pipe(rename({suffix: ".min"}))
+      .pipe(gulp.dest('public/js'));
+  });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-  gulp.watch('public/sass/*.{sass,scss}', ['compass']);
-  gulp.watch('public/js/podium/*.js', ['lint', 'scripts']);
-});
+  gulp.task('watch', function() {
+    gulp.watch('public/sass/*.{sass,scss}', ['compass']);
+    gulp.watch('public/js/podium/*.js', ['lint', 'scripts']);
+  });
 
-// Default Task
 gulp.task('build', ['moveFonts', 'compass', 'lint', 'scripts']);
 gulp.task('default', ['build', 'watch']);
